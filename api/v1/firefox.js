89150702b8c1a41878ea0536e82a18607b7bd3ff
Avirup Ghosh <ag.avirup1997@gmail.com>
2021-10-29 05:13:25 +0530

    added firefox plugin api

---
+++ api/v1/firefox.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const getStoreUrl = require('../../utils/storeUrl')
+
+const fetchData = async (request_url) => {
+	const res = await axios(request_url)
+	if (!res) return
+	const $ = cheerio.load(res.data)
+
+	let data = $('script[type="application/ld+json"][data-react-helmet="true"]').html()
+	if (!data) return
+	const d = JSON.parse(decodeURIComponent(data))
+
+	let author = {}
+	author.name = $('.AddonTitle-author>a').text()
+	author.url = $('.AddonTitle-author>a').attr('href')
+	if (!author.url?.match(/https?\:\/\//))
+		author.url = `https://addons.mozilla.org${author.url}`
+
+	return {
+		title: d.name,
+		description: d.description,
+		image: d.image,
+		url: d.url,
+		category: 'plugin',
+		author,
+		price: 'Free',
+		rating: parseFloat(d.aggregateRating.ratingValue).toFixed(1).toString(),
+		version: d.version,
+	}
+}
+
+module.exports = async (req, res) => {
+	const { id, regn } = req.query
+	if (!id) res.status(404).end('ID is not given, can not process request')
+
+	const request_url = getStoreUrl('firefox', id, regn)
+	const result = await fetchData(request_url)
+
+	if (result) res.send(result)
+	else res.status(404).send('Unable to process request')
+}
